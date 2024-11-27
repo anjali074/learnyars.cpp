1cd3225f7d54e529e4a66b39264dd37bfe552f64
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-11-27 11:37:34 +0530

    grace mark

--- file3.cpp
+++ file3.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 int main(){
   //input marks of 5 student and print the avg marks
+  //give all of them grace marking incerease the their marks by 5%
 float markstudent[5];
  for(int i=0; i<5; i++){
     cin>>markstudent[i];
   }
   float sum=0;
   for(int i=0; i<5; i++){
+    markstudent[i]=markstudent[i]*5/100;
+    cout<<markstudent[i];
     sum+=markstudent[i];
     
   }
